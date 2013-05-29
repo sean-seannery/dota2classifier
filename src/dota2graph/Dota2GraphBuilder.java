2c322f01063c978aaa46446f270cf7c32bc6bcc1
sean-seannery <sean@cs.ucsb.edu>
2013-05-29 02:34:25 -0700

    basic eclipse project structure

---
+++ src/dota2graph/Dota2GraphBuilder.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package dota2graph;
+
+/**
+ * @author sam
+ *
+ */
+public class Dota2GraphBuilder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/dota2parser/ReplayParser.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package dota2parser;
+
+/**
+ * @author sam
+ *
+ */
+public class ReplayParser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//provide log file directory
+		//create dictionary of boring keywords
+		//loop through directory
+			//loop through file
+				//if contains boring keyword, then remove line
+				//write line to new file
+
+	}
+
+}
